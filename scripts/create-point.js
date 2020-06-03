69933bbb43b339e67407f551dee6225cba1b5403
Joao Amancio Ribeiro Junior <joao.junior@ascom.cidadania.gov.br>
2020-06-03 19:08:25 -0300

    corrigndo bug

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -4,50 +4,58 @@ function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( res => res.json() )
-    .then( states => {
+        .then(res => res.json())
+        .then(states => {
 
-        for (const state of states){
-            ufSelect.innerHTML += `<option value="${state.id}">${state.name}</option>`
+            for (const state of states) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
             }
-    })
+        })
 }
 
-populateUFs() 
+populateUFs()
 
-function getCities (event) {
-    const citySelect=  document.querySelector("[nome=city]")
-    const stateInput=  document.querySelector("[nome=state]")
+function getCities(event) {
+    const citySelect = document.querySelector("[nome=city]")
+    const stateInput = document.querySelector("[nome=state]")
 
-    const ufValue = event.target.value
+    const ufValue = event.target.Value
 
     const indexOfSelectedState = event.target.selectedIndex
     stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
-    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.innerHTML = "<option value=>Selecione a Cidade</option>"
     citySelect.disabled = true
 
 
     fetch(url)
-    .then( res => res.json() )
-    .then( cities => {
+        .then(res => res.json())
+        .then(cities => {
 
-        for ( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.nome}">${city.name}</option>`
+            for (const city of cities) {
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
 
             citySelect.disabled = false
-    } )
+        })
 }
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
 
 
+// Itens de coleta
+//pegar todos os li' s
+const ItemsToCollect = document.querySelectorAll(".items-grid li")
 
-    // Itens de coleta
+for (const item of ItemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+function handleSelectedItem(event) {
+    console.log(event.target)
+}
 
 
 
